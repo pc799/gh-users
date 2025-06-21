22d15c0d5e99d6fbc01b30388160d7a2dc2dd777
Praveen Raj <praveenraj7@outlook.com>
2025-06-21 08:47:25 +0000

    feat: add honeycomb effect to grid (WIP: responsiveness)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { fetchUsers } from "@/app/lib/data";
 import Profile from "@/app/ui/profile";
 
@@ -7,10 +8,21 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">GitHub Users</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {users.map((user) => (
-            <Profile key={user.id} user={user} />
-          ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-30 gap-y-0">
+          {users.map((user, i) => {
+            // TODO: responsive
+            const isOddRow = Math.floor(i / 3) % 2 === 1;
+            return (
+              <div
+                key={user.id}
+                className={clsx("-mb-[100px]", {
+                  "translate-x-[160px]": isOddRow,
+                })}
+              >
+                <Profile user={user} />
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
