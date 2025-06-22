51bb31a8270d26cb2f6266934b15d6ddd34f5cd4
Praveen Raj <praveenraj7@outlook.com>
2025-06-22 11:06:16 +0000

    refactor: add size prop to Profile component for dynamic size

--- src/app/ui/profile.tsx
+++ src/app/ui/profile.tsx
@@ -3,19 +3,24 @@ import { User } from "@/app/lib/definitions";
 
 interface ProfileProps {
   user: User;
+  size: number;
 }
 
-export default function Profile({ user }: ProfileProps) {
+export default function Profile({ user, size }: ProfileProps) {
   return (
-    <div className="relative w-[200px] h-[200px] flex items-end overflow-hidden">
+    <div
+      className={`relative w-[${size}px] h-[${size}px] flex items-end overflow-hidden`}
+    >
       <Image
         src={user.avatar_url}
         alt={user.login}
-        width={200}
-        height={200}
+        width={size}
+        height={size}
         className="hexagon"
       />
-      <p className="absolute bottom-0 left-0 w-full max-w-[120px] px-2 text-center text-white text-base m-0 ms-10 bg-black bg-opacity-60 truncate whitespace-nowrap pointer-events-none">
+      <p
+        className={`absolute bottom-0 left-0 w-full max-w-[${size * 0.6}px] px-2 text-center text-white text-base m-0 ms-${size * 0.05} bg-black bg-opacity-60 truncate whitespace-nowrap pointer-events-none`}
+      >
         {user.login}
       </p>
     </div>
