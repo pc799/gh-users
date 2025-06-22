3853596900163fb5cf9b4e20a268eafd581b9c06
Praveen Raj <praveenraj7@outlook.com>
2025-06-22 11:48:07 +0000

    refactor: move user details to Profile component

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,7 +1,6 @@
 import { fetchUserData } from "@/app/lib/data";
 import { Suspense } from "react";
-import Avatar from "@/app/ui/avatar";
-import Card from "@/app/ui/card";
+import Profile from "@/app/ui/profile";
 
 export default async function Page({
   params,
@@ -9,11 +8,7 @@ export default async function Page({
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params;
-  const userData = await fetchUserData(username);
-
-  if (!userData) {
-    return <div>User not found!</div>;
-  }
+  const userData = fetchUserData(username);
 
   return (
     <div className="bg-[url(/hexagons.svg)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -21,43 +16,7 @@ export default async function Page({
         <h1 className="text-4xl font-bold">GH Bees</h1>
         <div className="w-full max-w-md">
           <Suspense fallback={<div>Loading...</div>}>
-            <div className="flex justify-center w-full">
-              <Avatar user={userData} size={400} />
-            </div>
-
-            <div className="mt-3 text-center">
-              <a
-                href={userData.html_url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-4xl font-bold hover:text-yellow-500 hover:underline"
-              >
-                <Card details={userData.name} />
-              </a>
-
-              <Card icon="/pencil.svg" details={userData.bio} />
-              <Card
-                icon="/code-bracket-square.svg"
-                details={`${userData.public_repos} repositories · ${userData.public_gists} gists`}
-              />
-              <Card
-                icon="/user-group.svg"
-                details={`${userData.followers} followers · ${userData.following} following`}
-              />
-              <Card icon="/building-office.svg" details={userData.company} />
-              <Card icon="/map-pin.svg" details={userData.location} />
-              <Card icon="/link.svg" details={userData.blog} />
-              <Card icon="x-mark.svg" details={userData.twitter_username} />
-              <Card
-                icon="/calendar-days.svg"
-                details={`joined ${new Date(
-                  userData.created_at,
-                ).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                })}`}
-              />
-            </div>
+            <Profile userData={userData} />
           </Suspense>
         </div>
       </main>
---
+++ src/app/ui/profile.tsx
@@ -0,0 +1,59 @@
+import Avatar from "@/app/ui/avatar";
+import Card from "@/app/ui/card";
+import { UserData } from "@/app/lib/definitions";
+import { use } from "react";
+
+interface ProfileProps {
+  userData: Promise<UserData | null>;
+}
+
+export default function Profile({ userData }: ProfileProps) {
+  let user = use(userData);
+
+  if (!user) {
+    return <div>User not found!</div>;
+  }
+
+  return (
+    <div>
+      <div className="flex justify-center w-full">
+        <Avatar user={user} size={400} />
+      </div>
+
+      <div className="mt-3 text-center">
+        <a
+          href={user.html_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-4xl font-bold hover:text-yellow-500 hover:underline"
+        >
+          <Card details={user.name} />
+        </a>
+
+        <Card icon="/pencil.svg" details={user.bio} />
+        <Card
+          icon="/code-bracket-square.svg"
+          details={`${user.public_repos} repositories · ${user.public_gists} gists`}
+        />
+        <Card
+          icon="/user-group.svg"
+          details={`${user.followers} followers · ${user.following} following`}
+        />
+        <Card icon="/building-office.svg" details={user.company} />
+        <Card icon="/map-pin.svg" details={user.location} />
+        <Card icon="/link.svg" details={user.blog} />
+        <Card icon="x-mark.svg" details={user.twitter_username} />
+        <Card
+          icon="/calendar-days.svg"
+          details={`joined ${new Date(user.created_at).toLocaleDateString(
+            "en-US",
+            {
+              year: "numeric",
+              month: "long",
+            },
+          )}`}
+        />
+      </div>
+    </div>
+  );
+}
