0c04b0f1c686064f5696fb0780b81b9337567d84
Praveen Raj <praveenraj7@outlook.com>
2025-06-22 16:09:41 +0000

    feat: add links to avatar for navigation

--- src/app/ui/avatar.tsx
+++ src/app/ui/avatar.tsx
@@ -1,30 +1,34 @@
 import Image from "next/image";
 import { User } from "@/app/lib/definitions";
+import Link from "next/link";
 
 interface AvatarProps {
   user: User;
-  scale: number;
+  scale: 1 | 2;
 }
 
 export default function Avatar({ user, scale }: AvatarProps) {
   const size = 200 * scale;
+  const maxWidth = 100 * scale;
+  const paddingBottom = 15 * scale;
+  const marginStart = 50 * scale;
 
   return (
-    <div
-      className={`relative w-[${size}px] h-[${size}px] flex items-end overflow-hidden`}
+    <Link
+      href={`/${user.login}`}
+      className={`hexagon relative w-[${size}px] h-[${size}px] flex items-end overflow-hidden`}
     >
       <Image
         src={user.avatar_url}
         alt={user.login}
         width={size}
         height={size}
-        className="hexagon"
       />
       <p
-        className={`absolute bottom-0 left-0 w-full max-w-[${size / 2}px] p-0 text-center m-0 ms-[${size / 4}px] bg-black/30 backdrop-blur-xs truncate whitespace-nowrap`}
+        className={`absolute bottom-0 left-0 w-full max-w-[${maxWidth}px] p-0 pb-[${paddingBottom}px] m-0 ms-[${marginStart}px] bg-black/30 backdrop-blur-xs truncate whitespace-nowrap rounded-t-xl font-bold text-center`}
       >
         {user.login}
       </p>
-    </div>
+    </Link>
   );
 }
