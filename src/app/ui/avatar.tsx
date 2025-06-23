1c832e5b65dbf39d2257d2f0cc9e5eaf4a586fee
Praveen Raj <praveenraj7@outlook.com>
2025-06-23 07:15:38 +0000

    fix: tailwind dynamic class bug

--- src/app/ui/avatar.tsx
+++ src/app/ui/avatar.tsx
@@ -9,14 +9,23 @@ interface AvatarProps {
 
 export default function Avatar({ user, scale }: AvatarProps) {
   const size = 200 * scale;
-  const maxWidth = 100 * scale;
-  const paddingBottom = 15 * scale;
-  const marginStart = 50 * scale;
+  const maxWidth = {
+    1: "max-w-[100px]",
+    2: "max-w-[200px]",
+  } as const;
+  const paddingBottom = {
+    1: "pb-[15px]",
+    2: "pb-[30px]",
+  } as const;
+  const marginStart = {
+    1: "ms-[50px]",
+    2: "ms-[100px]",
+  } as const;
 
   return (
     <Link
       href={`/${user.login}`}
-      className={`hexagon relative w-[${size}px] h-[${size}px] flex items-end overflow-hidden hover:scale-110 transition-transform duration-300 ease-out`}
+      className={`hexagon relative flex items-end overflow-hidden hover:scale-110 transition-transform duration-300 ease-out`}
     >
       <Image
         src={user.avatar_url}
@@ -25,7 +34,7 @@ export default function Avatar({ user, scale }: AvatarProps) {
         height={size}
       />
       <p
-        className={`absolute bottom-0 left-0 w-full max-w-[${maxWidth}px] p-0 pb-[${paddingBottom}px] m-0 ms-[${marginStart}px] bg-black/30 backdrop-blur-xs truncate whitespace-nowrap rounded-t-xl font-bold text-center`}
+        className={`absolute bottom-0 left-0 w-full ${maxWidth[scale]} p-0 ${paddingBottom[scale]} m-0 ${marginStart[scale]} bg-black/30 backdrop-blur-xs truncate whitespace-nowrap rounded-t-xl font-bold text-center`}
       >
         {user.login}
       </p>
